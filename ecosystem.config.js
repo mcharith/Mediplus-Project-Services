726afaa25af8d5077ac1641d25cf68f4e5c8f82f
Charith Mihiranga <156079886+mcharith@users.noreply.github.com>
2026-03-23 15:44:07 +0530

    Add cloud-sql-auth-proxy to PM2 ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,22 +6,22 @@ module.exports = {
           log_file: "./logs/cloud-sql-proxy.log",
       },
       {
-        name   : "patient-service",
-        script : "java -jar ./patient-service/target/Patient-Service-0.0.1-SNAPSHOT.jar",
-        log_file: "./logs/patient-service.log",
-        instances: 2,
+          name   : "patient-service",
+          script : "java -jar ./patient-service/target/Patient-Service-0.0.1-SNAPSHOT.jar",
+          log_file: "./logs/patient-service.log",
+          instances: 2,
       },
       {
-        name   : "doctor-service",
-        script : "java -jar ./doctor-service/target/doctor-service-0.0.1-SNAPSHOT.jar",
-        log_file: "./logs/doctor-service.log",
-        instances: 2,
+          name   : "doctor-service",
+          script : "java -jar ./doctor-service/target/doctor-service-0.0.1-SNAPSHOT.jar",
+          log_file: "./logs/doctor-service.log",
+          instances: 2,
       },
       {
-        name   : "appointment-service",
-        script : "java -jar ./appointment-service/target/appointment-service-0.0.1-SNAPSHOT.jar",
-        log_file: "./logs/appointment-service.log",
-        instances: 2,
+          name   : "appointment-service",
+          script : "java -jar ./appointment-service/target/appointment-service-0.0.1-SNAPSHOT.jar",
+          log_file: "./logs/appointment-service.log",
+          instances: 2,
       }
   ]
 }
